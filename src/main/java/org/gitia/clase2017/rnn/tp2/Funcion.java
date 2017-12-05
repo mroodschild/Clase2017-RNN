b5d3a767d5bc96a240da47c1e1516b40314f0ce2
MATIAS-PCG1\mroodschild <mroodschild@gmail.com>
2017-12-05 06:15:40 -0300

    Actualización de la Normalización en Funcion.java

--- src/main/java/org/gitia/clase2017/rnn/tp2/Funcion.java
+++ src/main/java/org/gitia/clase2017/rnn/tp2/Funcion.java
@@ -34,6 +34,7 @@ import org.gitia.froog.lossfunction.LossFunction;
 import org.gitia.froog.trainingalgorithm.Backpropagation;
 import org.gitia.froog.transferfunction.TransferFunction;
 import org.gitia.jdataanalysis.CSV;
+import org.gitia.jdataanalysis.data.stats.STD;
 
 /**
  *
@@ -42,38 +43,44 @@ import org.gitia.jdataanalysis.CSV;
 public class Funcion {
 
     public static void main(String[] args) {
-            SimpleMatrix input = CSV.open("src/main/resources/function/train_in.csv");
-            SimpleMatrix output = CSV.open("src/main/resources/function/train_out.csv");
-            SimpleMatrix in_test = CSV.open("src/main/resources/function/test_in.csv");
-            SimpleMatrix out_test = CSV.open("src/main/resources/function/test_out.csv");
-            SimpleMatrix all_in = CSV.open("src/main/resources/function/all_in.csv");
-            
-            int inputSize = input.numCols();
-            int outputSize = output.numCols();
-            
-            //==================== Preparamos la RNA =======================
-            Random rand = new Random(1);
-            int nn = 8;
-            Feedforward net = new Feedforward();
-            net.addLayer(new Layer(inputSize, nn, TransferFunction.TANSIG, rand));
-            net.addLayer(new Layer(nn, outputSize, TransferFunction.PURELIM, rand));
-            
-            //=================  configuraciones del ensayo ========================
-            // Preparamos el algoritmo de entrenamiento
-            Backpropagation bp = new Backpropagation();
-            bp.setEpoch(1000);
-            bp.setMomentum(0.9);
-            bp.setLearningRate(0.01);
-            bp.setInputTest(in_test);
-            bp.setOutputTest(out_test);
-            bp.setTestFrecuency(1);
-            bp.setLossFunction(LossFunction.RMSE);
-            
-            input.printDimensions();
-            output.printDimensions();
-            bp.entrenar(net, input, output);
-            
-        try{
+        SimpleMatrix input = CSV.open("src/main/resources/function/train_in.csv");
+        SimpleMatrix output = CSV.open("src/main/resources/function/train_out.csv");
+        SimpleMatrix in_test = CSV.open("src/main/resources/function/test_in.csv");
+        SimpleMatrix out_test = CSV.open("src/main/resources/function/test_out.csv");
+        SimpleMatrix all_in = CSV.open("src/main/resources/function/all_in.csv");
+
+        STD std = new STD();
+        std.fit(input);
+        input = std.eval(input);
+        in_test = std.eval(in_test);
+        all_in = std.eval(all_in);
+        
+        int inputSize = input.numCols();
+        int outputSize = output.numCols();
+
+        //==================== Preparamos la RNA =======================
+        Random rand = new Random(4);
+        int nn = 6;
+        Feedforward net = new Feedforward();
+        net.addLayer(new Layer(inputSize, nn, TransferFunction.TANSIG, rand));
+        net.addLayer(new Layer(nn, outputSize, TransferFunction.PURELIM, rand));
+
+        //=================  configuraciones del ensayo ========================
+        // Preparamos el algoritmo de entrenamiento
+        Backpropagation bp = new Backpropagation();
+        bp.setEpoch(13000);
+        bp.setMomentum(0.9);
+        bp.setLearningRate(0.01);
+        bp.setInputTest(in_test);
+        bp.setOutputTest(out_test);
+        bp.setTestFrecuency(1);
+        bp.setLossFunction(LossFunction.RMSE);
+
+        input.printDimensions();
+        output.printDimensions();
+        bp.entrenar(net, input, output);
+
+        try {
             net.outputAll(all_in).saveToFileCSV("src/main/resources/function/res_train.csv");
         } catch (IOException ex) {
             Logger.getLogger(Funcion.class.getName()).log(Level.SEVERE, null, ex);
